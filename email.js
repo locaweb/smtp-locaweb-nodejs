dac6882601c7c0f7348a2e4e23f94b14cf947650
Kemel Zaidan <kemelzaidan@gmail.com>
2015-05-19 18:14:51 -0300

    Added email.js

---
+++ email.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var request = require('request');
+
+var endpoint = 'https://api.smtplw.com.br/v1/messages';
+
+var email = new function(param) {
+    param = param || {};
+
+    this.subject    = param.subject     || '';
+    this.from       = param.from        || '';
+    this.to         = param.to          || [];
+    this.cc         = param.to          || [];
+    this.bcc        = param.bcc         || [];
+    this.body       = param.body        || '';
+    this.headers    = param.headers     || {};
+    this.apiToken   = param.token       || '';
+
+}
+
+email.prototype.addToken = function(token) {
+    this.apiToken = token;
+    this.headers.x-auth-token = this.token;
+}
+
