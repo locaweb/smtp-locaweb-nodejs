11e8f5efb601b4f590a6fcacc54ec78d1dd03813
Kemel Zaidan <kemelzaidan@gmail.com>
2015-05-25 19:30:58 -0300

    refactorin on smtp-locaweb.js e test.js

--- smtp-locaweb.js
+++ smtp-locaweb.js
@@ -1,16 +1,17 @@
 var request = require('request');
-var email = require('email.js');
+var email = require('./email.js');
 
+var endpoint = 'https://api.smtplw.com.br/v1/messages';
 var options = {
-    endpoint: 'https://api.smtplw.com.br/v1/messages',
     headers: {
         'x-auth-token': ''
     }
+};
 
-var sendMail = function(emailObject){
-    request.post({url:endpoint, form:emailObject}, function(err, httpResponse, body){
+module.exports = function (emailObject){
+    request.post(options, {url:endpoint, form:emailObject}, function(err, httpResponse, body){
         if (err) { return console.error(err);}
         console.log("HTTP Code: " + httpResponse);
         console.log("Response body: " + body);
-    }
-}
+    });
+};
