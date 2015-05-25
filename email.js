f9a605aee38f4a5357adb0781c0aa8dad2fbdfb0
Kemel Zaidan <kemelzaidan@gmail.com>
2015-05-25 19:26:17 -0300

    refactoring on email.js

--- email.js
+++ email.js
@@ -1,48 +1,45 @@
-// var http = require('http');
-// var request = require('request');
+'use strict';
+var method = Email.prototype;
 
-var email = new function(param){
+function Email (param) {
     param = param || {};
-
-    this.subject    = param.subject     || '';
-    this.from       = param.from        || '';
-    this.to         = param.to          || [];
-    this.cc         = param.to          || [];
-    this.bcc        = param.bcc         || [];
-    this.body       = param.body        || '';
-    this.headers    = param.headers     || {};
-    this.apiToken   = param.token       || '';
-
+    this.subject    = param.subject || '';
+    this.from       = param.from    || '';
+    this.to         = param.to      || [];
+    this.cc         = param.cc      || [];
+    this.bcc        = param.bcc     || [];
+    this.body       = param.body    || '';
+    this.headers    = param.headers || {};
+    this.apiToken   = param.token   || '';
 }
 
-
-email.prototype.addSubject = function(subject){
+method.addSubject = function (subject) {
     this.subject = subject;
 };
 
-email.prototype.addFrom = function(from){
+method.addFrom = function (from) {
     this.from = from;
 };
 
-email.prototype.addTo = function(to){
-    this.to = to;    
+method.addTo = function (to) {
+    this.to = to;
 };
 
-email.prototype.addCc = function(cc){
-    this.cc = cc;    
+method.addCc = function (cc) {
+    this.cc = cc;
 };
 
-email.prototype.addBcc = function(bcc){
-    this.bcc = bcc;    
+method.addBcc = function (bcc) {
+    this.bcc = bcc;
 };
 
-email.prototype.addBody = function(body){
+method.addBody = function (body) {
     this.body = body;
 };
 
-//email.prototype.addToken = function(token){
+//method.addToken = function(token){
 //    this.apiToken = token;
 //    this.headers.x-auth-token = this.token;
 //}
 
-module.exports = email;
+module.exports = Email;
