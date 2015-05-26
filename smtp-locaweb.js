93fecaca628abe1ec1970284f029822227831494
Kemel Zaidan <kemelzaidan@gmail.com>
2015-05-26 19:37:43 -0300

    changing file to use the token.js file with credentials

--- smtp-locaweb.js
+++ smtp-locaweb.js
@@ -1,17 +1,23 @@
 var request = require('request');
 var email = require('./email.js');
+var token = require('./token.js');
 
-var endpoint = 'https://api.smtplw.com.br/v1/messages';
-var options = {
-    headers: {
-        'x-auth-token': ''
-    }
-};
+//var options = {
+//        headers: {
+//            'x-auth-token': '952bf9d71decb71dfba71d6e9508eb60'
+//        },
+//        rejectUnauthorized: false,
+//        url: 'https://api.smtplw.com.br/v1/messages'
+//}
 
 module.exports = function (emailObject){
-    request.post(options, {url:endpoint, form:emailObject}, function(err, httpResponse, body){
+    request.post({headers: {
+            'x-auth-token': token
+        },
+        rejectUnauthorized: false,
+        url: 'https://api.smtplw.com.br/v1/messages', form:emailObject}, function(err, httpResponse, body){
         if (err) { return console.error(err);}
-        console.log("HTTP Code: " + httpResponse);
+        console.log("HTTP Code: " + httpResponse.statusCode);
         console.log("Response body: " + body);
     });
 };
