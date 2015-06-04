b16d39791b91d554f1d848aa3f4e3fd33338a537
Kemel Zaidan <kemel.zaidan@locaweb.com.br>
2015-06-03 22:40:39 -0300

    add example usage file

---
+++ example_usage.js
@@ -0,0 +1,24 @@
+var locaweb = require('smtp-locaweb-nodejs');
+
+var email = new locaweb.Email();
+
+email.addTo('email_address@provider.com');
+email.addSubject('Email title!!!');
+email.addFrom('noreply@yourdomain.com');
+email.addBody('A cool and useful content.');
+email.addCc('optional@address.com');
+
+locaweb.sendMail(email);
+
+// another option is passing an object with all the right atributes to the constructor
+message = {
+    to: 'email_address@provider.com',
+    subject: 'Email title!!!',
+    from: 'noreply@yourdomain.com',
+    body: 'A cool and useful content.',
+    cc: 'optional@address.com'
+}
+
+var email2 = new locaweb.Email(message);
+
+locaweb.sendMail(email2);
